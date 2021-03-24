953d3034e7ac2c2d6de4c2d0345447da1e7f2181
Richard Ball <webalation@gmail.com>
2021-03-24 07:30:05 -0400

    Handling click events

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "Correct Number! 🎉";
 console.log(document.querySelector(".message").textContent);
@@ -9,3 +10,17 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️  No number!";
+  }
+});
