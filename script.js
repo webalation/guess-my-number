2d7e7c223dd3f4f4cee1e122e812b98fdd539fbf
Richard Ball <webalation@gmail.com>
2021-03-25 07:16:41 -0400

    Implementing the game logic

--- script.js
+++ script.js
@@ -2,25 +2,46 @@
 
 /*
 console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number! 🎉";
+document.querySelector(".message").textContent = "Correct secretNumber! 🎉";
 console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".number").textContent = 13;
+document.querySelector(".secretNumber").textContent = 13;
 document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
 
-const x = function () {
-  console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(typeof guess, guess);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️  No number!";
+    document.querySelector(".message").textContent = "No number! ⛔️";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number! 🎉";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high! 📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game! 💥";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low! 📉";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game! 💥";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
