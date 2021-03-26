3743a061b47b99d100d95a2c40f42934c58e052e
Richard Ball <webalation@gmail.com>
2021-03-26 06:57:24 -0400

    Manipulating CSS styles

--- script.js
+++ script.js
@@ -21,10 +21,19 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(typeof guess, guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "No number! ⛔️";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number! 🎉";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high! 📈";
@@ -34,6 +43,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "You lost the game! 💥";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too low! 📉";
